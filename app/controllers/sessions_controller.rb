23f5a74afd8b63b399fec0f2b930860cf04f8008
jwatts82 <jessperr182@hotmail.com>
2020-11-17 15:00:37 -0800

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create 
+    # binding.pry
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id  
+    else  
+      redirect_to '/login'
+    end
+  end 
+end
