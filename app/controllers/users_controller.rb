50fa060513f55de3f3e400a06c64bec8d649dd81
doosemavis <moosedavis2011@gmail.com>
2020-11-25 15:09:31 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+    def show 
+        @user = User.find(session[:user_id])
+    end 
+    
+    def new
+        @user = User.new
+    end 
+
     def create 
         @user = User.new(user_params)
         if @user.save
